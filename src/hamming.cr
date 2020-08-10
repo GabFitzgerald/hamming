4b6e62ed3a3f8c9c2c6c443754343de18850d7cc
GabFitzgerald <gabrielle.ma.fitzgerald@gmail.com>
2020-08-10 14:20:03 +1000

    all tests passing

--- src/hamming.cr
+++ src/hamming.cr
@@ -1,1 +1,18 @@
 # Please implement your solution to hamming in this file
+class Hamming
+    def self.distance(strand1, strand2)
+        unless strand1.size == strand2.size 
+            raise ArgumentError.new "Strands are not the same size"
+        end
+        strand1chars = strand1.split("")
+        strand2chars = strand2.split("")
+        distance = 0
+        strand1chars.map_with_index { |char, index| 
+            unless char == strand2chars[index]
+                distance += 1
+            end 
+        }
+        return distance
+    end
+end
+
